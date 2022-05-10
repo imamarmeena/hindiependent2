7f4170731329ee7807f848f67d2b042b104ad5c8
imamarmeena <43732879+imamarmeena@users.noreply.github.com>
2022-05-11 02:27:39 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Open your spotify dashboard
+ * https://beta.developer.spotify.com/dashboard
+ */
+
+const client_id = '49c57dcf6f464284b8cca9455cef86b0';
+const redirect_uri = 'localhost:8080';
+const client_secret = 'bd85805659af4628a57565c8f1b46e48';
